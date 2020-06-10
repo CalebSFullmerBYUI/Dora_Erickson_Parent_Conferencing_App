088f229d2921c8f2737885f85603378a248d55df
CalebFullmer <calebsf@byui.edu>
2020-06-10 15:18:15 -0600

    Added test for MessageItem class.

--- app/src/test/java/com/example/doraericksonparentconferencingapp/objectTest.java
+++ app/src/test/java/com/example/doraericksonparentconferencingapp/objectTest.java
@@ -3,13 +3,19 @@ package com.example.doraericksonparentconferencingapp;
 import org.junit.Test;
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.lang.reflect.Field;
+import java.util.List;
 
 /**
  * Meant to be used for testing various objects.
  */
 public class objectTest {
+
+    /**
+     * Does testing for the TextInformation class.
+     */
     @Test
     public void testTextInfo() {
         Field testSender = null;
@@ -100,6 +106,88 @@ public class objectTest {
             assert(testSentDate.get(testTextInfo).equals(new Date(100)));
             testTextInfo.setDate(null);
             assert(testSentDate.get(testTextInfo).equals(new Date(0)));
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+            //Test has failed.
+            assert(false);
+        }
+    }
+
+
+    /**
+     * Preformes testing for the MessageItem class.
+     */
+    @Test
+    public void testMessageItem() {
+        Field testRecipient = null;
+        Field testReplies = null;
+        Field testIsRead = null;
+
+        try {
+            testRecipient = MessageItem.class.getDeclaredField("recipient");
+            testRecipient.setAccessible(true);
+            testReplies = MessageItem.class.getDeclaredField("replies");
+            testReplies.setAccessible(true);
+            testIsRead = MessageItem.class.getDeclaredField("isRead");
+            testIsRead.setAccessible(true);
+
+            //Test default constructor.
+            MessageItem testTextInfo = new MessageItem();
+            assert (testRecipient.get(testTextInfo).equals(""));
+            assert (testReplies.get(testTextInfo).equals(new ArrayList<MessageItem>()));
+            assert (testIsRead.get(testTextInfo) == false);
+
+
+            //Test overload constructor.
+            testTextInfo = new MessageItem("TestSender", "Hi there.", new Date(8000), "Hi");
+            assert (testRecipient.get(testTextInfo).equals("Hi"));
+            assert (testReplies.get(testTextInfo).equals(new ArrayList<MessageItem>()));
+            assert (testIsRead.get(testTextInfo) == false);
+
+            testTextInfo = new MessageItem(null, null, null, null);
+            assert (testRecipient.get(testTextInfo).equals(""));
+            assert (testReplies.get(testTextInfo).equals(new ArrayList<MessageItem>()));
+            assert (testIsRead.get(testTextInfo) == false);
+
+
+            //Test copy constructor
+            testTextInfo = new MessageItem(new MessageItem("TestSender", "Hi there.", new Date(8000), "Bye"));
+            assert (testRecipient.get(testTextInfo).equals("Bye"));
+            assert (testReplies.get(testTextInfo).equals(new ArrayList<MessageItem>()));
+            assert (testIsRead.get(testTextInfo) == false);
+
+
+
+            //Test getters
+            testRecipient.set(testTextInfo, "sup");
+            assert (testTextInfo.getRecipient().equals("sup"));
+            testRecipient.set(testTextInfo, null);
+            assert (testTextInfo.getRecipient().equals(""));
+
+            testReplies.set(testTextInfo, new ArrayList<MessageItem>(6));
+            assert(testTextInfo.getReplies().equals(new ArrayList<MessageItem>(6)));
+            testReplies.set(testTextInfo, null);
+            assert(testTextInfo.getReplies() != null);
+
+            testIsRead.set(testTextInfo, true);
+            assert(testTextInfo.getIsRead() == true);
+
+
+            //Test setters
+            testTextInfo.setRecipient("Dana");
+            assert(testRecipient.get(testTextInfo).equals("Dana"));
+            testTextInfo.setSender(null);
+            assert(testRecipient.get(testTextInfo).equals(""));
+
+            testTextInfo.setIsRead(false);
+            assert(testRecipient.get(testTextInfo).equals(false));
+
+            MessageItem testMessageItem = new MessageItem();
+            testTextInfo.addReply(testMessageItem);
+            List<MessageItem> messReplies = (ArrayList<MessageItem>)testReplies.get(testTextInfo);
+            assert(messReplies.get(0).equals(testMessageItem));
+            testTextInfo.addReply(null);
+            assert(((ArrayList<MessageItem>) testReplies.get(testTextInfo)).size() == 1);
         } catch (Exception e) {
             System.out.println(e.getLocalizedMessage());
             //Test has failed.
