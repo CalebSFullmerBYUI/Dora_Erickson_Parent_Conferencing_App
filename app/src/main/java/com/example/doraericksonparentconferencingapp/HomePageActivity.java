121e5795c4a40efb2d169dfb744f611d67f6926c
CalebFullmer <calebsf@byui.edu>
2020-06-22 16:35:41 -0600

    Wrote mock server responses for HomePageActivity server requests.

--- app/src/main/java/com/example/doraericksonparentconferencingapp/HomePageActivity.java
+++ app/src/main/java/com/example/doraericksonparentconferencingapp/HomePageActivity.java
@@ -28,7 +28,7 @@ import java.util.ArrayList;
 public class HomePageActivity extends AppCompatActivity {
     //Variables
     private User currentUser = null;
-    private List<ScheduleItem> announcements = new ArrayList<ScheduleItem>();
+    private ArrayList<ScheduleItem> announcements = new ArrayList<ScheduleItem>();
     public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("MMM d, yyyy 'at' hh:mm a");
     public ArrayAdapter<AnnouncementView> announceAdapter = new ArrayAdapter<AnnouncementView>(this,
             android.R.layout.simple_list_item_1, new ArrayList<AnnouncementView>());
@@ -41,37 +41,41 @@ public class HomePageActivity extends AppCompatActivity {
         //Set up adapter for announcements listview.
         ((ListView)findViewById(R.id.lstView_Announcements)).setAdapter(announceAdapter);
 
-        //Get user info
-        Thread startUp = new Thread(new CustomRun(this) {
-            @Override
-            public void run() {
-                //Gets the currentUser as a Json object.
-                //Add file path and variable keys.
-                final String userJson = new ServerRequest().request("", "");
+        if (MainActivity.UNIQUE_ID != null) {
+            //Get user info
+            Thread startUp = new Thread(new CustomRun(this) {
+                @Override
+                public void run() {
+                    //Gets the currentUser as a Json object.
+                    //Add file path and variable keys.
+                    final String userJson = new ServerRequest().request("", "");
 
-                currentActivity.get().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if ((userJson != null) && (!userJson.equals(""))) {
-                            //Set user and set announcements.
+                    currentActivity.get().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if ((userJson != null) && (!userJson.equals(""))) {
+                                //Set user and set announcements.
 
-                            //mockResponse used in place of userJson for prototype.
-                            String mockResponse = "{'announcements': [{'sender': 'admin1', 'subject':" +
-                                    "'New School Policy', 'message': 'We will be implementing a new" +
-                                    "school policy starting October 12, 2020. It requires students to" +
-                                    "be on time to class.', 'sentDate': '', 'dueDate': null, 'isHomework': false}, {}, {}, {}]}";
-                            currentUser = new Gson().fromJson(userJson, User.class);
-                            new Gson().
-                            refreshAnnouncements();
-                        } else {
-                            Toast errorToast = Toast.makeText(currentActivity.get().getApplicationContext(),
-                                    "Could not log-in. Please Restart App", Toast.LENGTH_LONG);
-                            errorToast.show();
+                                //mockResponse used in place of userJson for prototype.
+                                String mockResponse = "{'name': 'John Doe', 'email': 'JohnDoe@fakeEmail.com', " +
+                                        "'classroom': '', 'classId': 0, 'isAdmin': false, 'uniqueId': 'FakeId'}";
+
+                                currentUser = new Gson().fromJson(mockResponse /*userJson*/, User.class);
+
+                                //This is for testing admin and teacher accounts.
+                                //currentUser.setIsAdmin(true);
+
+                                refreshAnnouncements();
+                            } else {
+                                Toast errorToast = Toast.makeText(currentActivity.get().getApplicationContext(),
+                                        "Could not log-in. Please Restart App", Toast.LENGTH_LONG);
+                                errorToast.show();
+                            }
                         }
-                    }
-                });
-            }
-        });
+                    });
+                }
+            });
+        }
     }
 
 
@@ -90,7 +94,25 @@ public class HomePageActivity extends AppCompatActivity {
                 String responseJson = new ServerRequest().request("", "");
                 final HomePageActivity updatedAnnouncements;
                 if ((responseJson != null) && (!responseJson.equals(""))) {
-                     updatedAnnouncements = convertJson.fromJson(responseJson,
+                    //mockResponse used in place of userJson for prototype.
+                    String mockResponse = "{'announcements': [{'sender': 'admin1', 'subject': " +
+                            "'New School Policy', 'message': 'We will be implementing a new " +
+                            "school policy starting October 12, 2020. It requires students to " +
+                            "be on time to class.', 'sentDate': '1598652343', 'dueDate': 0, " +
+                            "'isHomework': false}," +
+                            "{'sender': 'admin1', 'subject': 'Welcome Back Event', 'message': " +
+                            "'The school will be hosting a welcome back event for the start of " +
+                            "the school year on September 4th.', 'sentDate': '1597356343', 'dueDate': " +
+                            "1599689143, 'isHomework': true}," +
+                            "{'sender': 'admin3', 'subject': 'PTO Meeting on August 24', 'message': " +
+                            "'We will be having a PTO meeting on August 24th to discuss the upcoming " +
+                            "school year.', 'sentDate': '1598652343', 'dueDate': 1598306743, " +
+                            "'isHomework': true}," +
+                            "{'sender': 'admin1', 'subject': 'Gradebook Changes', 'message': " +
+                            "'Starting September 4th, there will be changes to the Gradebook app. " +
+                            "Check PowerSchool for more details.', 'sentDate': '1598652343', " +
+                            "'dueDate': null, 'isHomework': false}]}";
+                     updatedAnnouncements = convertJson.fromJson(mockResponse/*responseJson*/,
                             HomePageActivity.class);
                 } else {
                     updatedAnnouncements = null;
