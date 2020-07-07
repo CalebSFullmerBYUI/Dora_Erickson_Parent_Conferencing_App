fc3b9399c8e3cf85347c94bdcea01277b3ec8560
Timothy Stephenson <ste17059@byui.edu>
2020-07-06 22:39:03 -0600

    scheduleItem added

---
+++ app/src/main/java/com/example/doraericksonparentconferencingapp/ScheduleItem.java
@@ -0,0 +1,33 @@
+package com.example.doraericksonparentconferencingapp;
+
+import java.util.Date;
+
+public class ScheduleItem {
+    private Date dueDate;
+    private boolean isHomework;
+
+    public ScheduleItem(Date dueDate, boolean isHomework){
+        dueDate = this.dueDate;
+        isHomework = this.isHomework;
+
+    }
+    public ScheduleItem (ScheduleItem scheduleItem){
+        scheduleItem = this;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+    public boolean getIsHomework(){
+        return isHomework;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setHomework(boolean homework) {
+        isHomework = homework;
+    }
+
+}
