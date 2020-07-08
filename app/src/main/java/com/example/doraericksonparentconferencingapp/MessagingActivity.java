e8a85ac2d1dbe27c47e8989e849e352f50e6588c
CalebFullmer <calebsf@byui.edu>
2020-07-08 10:22:08 -0600

    Completed Skeletons for MessagingActivity and NewMessageActivity.

--- app/src/main/java/com/example/doraericksonparentconferencingapp/MessagingActivity.java
+++ app/src/main/java/com/example/doraericksonparentconferencingapp/MessagingActivity.java
@@ -3,12 +3,65 @@ package com.example.doraericksonparentconferencingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class MessagingActivity extends AppCompatActivity {
+    private ArrayList<MessageItem> messages = new ArrayList<MessageItem>();
+    private User currentUser = null;
+    private boolean unreadOnly = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messaging);
+    }
+
+
+    /**
+     * <h3>getMessages()</h3>
+     * Retrieves messages from server.
+     */
+    public void getMessages() {
+
+    }
+
+    /**
+     * <h3>displayMessages</h3>
+     * Displays messages.
+     */
+    public void displayMessages() {
+
+    }
+
+
+
+
+    /**
+     * <h3>refreshMessages(View view)</h3>
+     * Manually cales getMessages.
+     * @param view (Type: View, the View which called the function)
+     */
+    public void refreshMessages(View view) {
+        getMessages();
+    }
+
+    /**
+     * <h3>newMessage(View view)</h3>
+     * Starts NewMessageActivity to create a new message.
+     * @param view (Type: View, the View which called the funcion)
+     */
+    public void newMessage(View view) {
+
+    }
+
+    /**
+     * <h3>replyToMessage(View view)</h3>
+     * Starts NewMessageActivity to reply to an existing message.
+     * @param view
+     */
+    public void replyToMessage(View view) {
+
     }
 }
--- app/src/main/java/com/example/doraericksonparentconferencingapp/NewMessageActivity.java
+++ app/src/main/java/com/example/doraericksonparentconferencingapp/NewMessageActivity.java
@@ -3,12 +3,44 @@ package com.example.doraericksonparentconferencingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class NewMessageActivity extends AppCompatActivity {
+    private MessageItem message = null;
+    private User currentUser = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_message);
+    }
+
+
+    /**
+     * <h3>send(View view)</h3>
+     * Sends the message to the server for dispersal.
+     * @param view (Type: View, the View which called the function)
+     */
+    public void send(View view) {
+
+    }
+
+    /**
+     * <h3>delete(View view)</h3>
+     * Checks if the user wants to delete the activity and, if so, ends the activity without
+     * saving.
+     * @param view (Type: View, the View which called the function)
+     */
+    public void delete(View view) {
+
+    }
+
+    /**
+     * <h3>save(View view)</h3>
+     * Saves the message to the server as a draft.
+     * @param view
+     */
+    public void Save(View view) {
+
     }
 }
